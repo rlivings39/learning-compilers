217d816dfa27e10b10cb54b6b2be5f2bb064589d
Ryan Livingston <43882944+rlivings39@users.noreply.github.com>
2025-06-26 12:17:14 -0400

    First go at AST definition. It probably sucks as I don't get the algebraic type idea yet.

---
+++ src/ast.test.ts
@@ -0,0 +1,18 @@
+import { test, expect } from "vitest";
+
+import * as ast from "./ast";
+
+test("AST construction", () => {
+  const numC = ast.NumericConstant(12);
+  const retS = ast.ReturnStmt(numC);
+  const func = ast.Function("main", retS);
+  const func2 = ast.Function("main", retS);
+  const prog = ast.Program(func);
+
+  expect(prog.function_definition).toBe(func);
+  expect(prog.function_definition).not.toBe(func2);
+  expect(func.name).toEqual("main");
+  expect(func.body).toBe(retS);
+  expect(retS.expr).toBe(numC);
+  expect(numC.value).toEqual(12);
+});
---
+++ src/ast.ts
@@ -0,0 +1,53 @@
+import { Statement } from "typescript";
+
+export type NumericConstant = {
+  kind: "numeric-const";
+  value: number;
+};
+
+export function NumericConstant(value: number): NumericConstant {
+  return { value, kind: "numeric-const" };
+}
+
+export type StringConstant = {
+  kind: "string-const";
+  value: string;
+};
+
+export function StringConstant(value: string): StringConstant {
+  return { kind: "string-const", value };
+}
+
+export type Constant = NumericConstant | StringConstant;
+
+export type Expr = Constant;
+
+export type ReturnStmt = {
+  kind: "return-stmt";
+  expr: Expr;
+};
+
+export function ReturnStmt(expr: Expr): ReturnStmt {
+  return { kind: "return-stmt", expr };
+}
+
+export type Stmt = ReturnStmt;
+
+export type Identifier = string;
+
+export type Function = {
+  name: Identifier;
+  body: Stmt;
+};
+
+export function Function(name: Identifier, body: Stmt): Function {
+  return { name, body };
+}
+
+export type Program = {
+  function_definition: Function;
+};
+
+export function Program(function_definition: Function): Program {
+  return { function_definition };
+}
