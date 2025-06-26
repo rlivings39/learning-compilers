9659bfd3d2b9c0d8f776696ad92a794a8143edd7
Ryan Livingston <43882944+rlivings39@users.noreply.github.com>
2025-06-26 14:20:59 -0400

    Basic pretty-printer

---
+++ src/pretty-print.test.ts
@@ -0,0 +1,14 @@
+import { test, expect } from "vitest";
+
+import * as ast from "./ast";
+import { prettyPrint } from "./pretty-print";
+
+test("Pretty printer", () => {
+  const numC = ast.NumericConstant(12);
+  const retS = ast.ReturnStmt(numC);
+  const func = ast.Function("main", retS);
+  const func2 = ast.Function("main", retS);
+  const prog = ast.Program(func);
+  const pp = prettyPrint(prog);
+  expect(pp).toEqual("");
+});
---
+++ src/pretty-print.ts
@@ -0,0 +1,47 @@
+import { Program, Function, Stmt, Expr, Constant } from "./ast";
+const INDENT_INCREMENT = 2;
+
+function printLine(line: string, indentLevel: number): string {
+  return " ".repeat(indentLevel) + line + "\n";
+}
+
+function printConstant(c: Constant): string {
+  switch (c.kind) {
+    case "numeric-const": {
+      return `${c.value}`;
+    }
+    case "string-const": {
+      return `${c.value}`;
+    }
+  }
+}
+function printExpr(exp: Expr): string {
+  return printConstant(exp);
+}
+function printStmt(stmt: Stmt, indentLevel: number): string {
+  let exp = printExpr(stmt.expr);
+  return printLine(`return ${exp};`, indentLevel);
+}
+
+function printFunction(func: Function, indentLevel: number): string {
+  let b = printStmt(func.body, indentLevel + INDENT_INCREMENT);
+  let ret = printLine(`Function ${func.name}() {`, indentLevel);
+  ret += b;
+  ret += printLine("}", indentLevel);
+  return ret;
+}
+
+function printProgram(program: Program, indentLevel: number): string {
+  let f = printFunction(
+    program.function_definition,
+    indentLevel + INDENT_INCREMENT
+  );
+  return `${printLine("Program (", indentLevel)}${f}${printLine(
+    ")",
+    indentLevel
+  )}`;
+}
+
+export function prettyPrint(ast: Program): string {
+  return printProgram(ast, 0);
+}
