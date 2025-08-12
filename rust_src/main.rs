6c4441789da7c2e2674d28b2277a88470de1af79
Ryan Livingston <43882944+rlivings39@users.noreply.github.com>
2025-08-11 23:00:08 -0400

    Simplify main

--- rust_src/main.rs
+++ rust_src/main.rs
@@ -3,6 +3,5 @@ use notcc;
 use std::env;
 fn main() -> Result<(), notcc::error::Error> {
   // TODO why mut?
-  notcc::driver_main(&mut env::args())?;
-  return notcc::error::fail("Bad news bro!");
+  notcc::driver_main(&mut env::args())
 }
