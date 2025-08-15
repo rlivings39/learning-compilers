694e1cf6c461f13a83add53b09f3065be4d4cc54
Ryan Livingston <43882944+rlivings39@users.noreply.github.com>
2025-08-15 08:48:05 -0400

    Stuck on borrow checker in parse_expr

--- rust_src/parse.rs
+++ rust_src/parse.rs
@@ -8,6 +8,38 @@ struct Parser<'a> {
   tokens: std::slice::Iter<'a, Token>,
 }
 
+fn check_token_exists(token: Option<&Token>) -> Result<&Token, Error> {
+  match token {
+    Some(t) => Ok(t),
+    None => Err("Unexpected end of file.".to_string()),
+  }
+}
+
+fn binop_precedence(token: &Token) -> Option<i32> {
+  match token.kind {
+    TokenKind::Assign => Some(1),
+    TokenKind::Question => Some(2),
+    TokenKind::Or => Some(5),
+    TokenKind::And => Some(10),
+    TokenKind::Equal => Some(30),
+    TokenKind::NotEqual => Some(30),
+    TokenKind::Less => Some(35),
+    TokenKind::LessEq => Some(35),
+    TokenKind::Greater => Some(35),
+    TokenKind::GreaterEq => Some(35),
+    TokenKind::Plus => Some(45),
+    TokenKind::Minus => Some(45),
+    TokenKind::Divide => Some(50),
+    TokenKind::Times => Some(50),
+    TokenKind::Remainder => Some(50),
+    _ => None,
+  }
+}
+
+fn is_binop(token: &Token) -> bool {
+  binop_precedence(token).is_some()
+}
+
 impl Parser<'_> {
   fn expect(&mut self, kind: TokenKind) -> Result<&Token, Error> {
     let token = match self.tokens.next() {
@@ -22,26 +54,20 @@ impl Parser<'_> {
   }
 
   fn take_token(&mut self) -> Result<(), Error> {
-    match self.tokens.next() {
-      Some(_) => Ok(()),
-      None => Err("Unexpected end of file.".to_string()), // TODO combine these messages
-    }
+    check_token_exists(self.tokens.next()).map(|_| ())
   }
 
-  fn peek_token(&mut self) -> Result<&Token, Error> {
+  fn peek_token(&self) -> Result<&Token, Error> {
     let mut iter = self.tokens.clone();
     let token = iter.next();
-    match token {
-      Some(t) => Ok(t),
-      None => Err("Unexpected end of file.".to_string()),
-    }
+    check_token_exists(token)
   }
 
   fn parse_block_item(&mut self) -> Result<ast::BlockItem, Error> {
     let next_token = self.peek_token()?;
     match next_token.kind {
       TokenKind::KwInt => self.parse_declaration(),
-      _ => self.parse_stmt(),
+      _ => self.parse_stmt().map(|stmt| ast::BlockItem::Stmt(stmt)),
     }
   }
 
@@ -74,8 +100,23 @@ impl Parser<'_> {
     }
   }
 
-  fn parse_expr(&mut self) -> Result<ast::Expr, Error> {
-    todo!()
+  // TODO check all parse_expr calls to make sure I have the right starting precedence
+  fn parse_expr(&mut self, min_precedence: i32) -> Result<ast::Expr, Error> {
+    let left = self.parse_factor()?;
+    let next_token = self.peek_token()?.clone();
+    let next_precedence = binop_precedence(next_token);
+    while let Some(next_precedence) = next_precedence
+      && next_precedence >= min_precedence
+    {
+      // match next_token.kind {
+      //   // TokenKind::Assign => {
+      //   //   self.take_token()?;
+      //   // }
+      //   // TokenKind::Question => {}
+      //   // _ => {}
+      // }
+    }
+    Err("".to_string())
   }
 
   fn parse_declaration(&mut self) -> Result<ast::BlockItem, Error> {
@@ -85,14 +126,54 @@ impl Parser<'_> {
     let init = match next_token.kind {
       TokenKind::Assign => {
         self.take_token()?;
-        Some(self.parse_expr()?)
+        Some(self.parse_expr(0)?)
       }
       _ => None,
     };
     Ok(ast::BlockItem::Declaration(name, init))
   }
 
-  fn parse_stmt(&self) -> Result<ast::BlockItem, Error> {
+  fn parse_stmt(&mut self) -> Result<ast::Stmt, Error> {
+    let next_token = self.peek_token()?;
+    match next_token.kind {
+      TokenKind::KwReturn => {
+        self.take_token()?;
+        let expr = self.parse_expr(0)?;
+        self.expect(TokenKind::Semicolon)?;
+        Ok(ast::Stmt::Return(expr))
+      }
+      TokenKind::Semicolon => {
+        self.take_token()?;
+        Ok(ast::Stmt::Null)
+      }
+      TokenKind::KwIf => {
+        self.take_token()?;
+        self.expect(TokenKind::LeftParen)?;
+        let cond = self.parse_expr(0)?;
+        self.expect(TokenKind::RightParen)?;
+        let true_stmt = self.parse_stmt()?;
+        let mut false_stmt: Option<ast::Stmt> = None;
+        // TODO need to check for existence? I think there must be another token after an if
+        let next_token = self.peek_token()?;
+        if let TokenKind::KwElse = next_token.kind {
+          self.take_token()?;
+          false_stmt = Some(self.parse_stmt()?);
+        }
+        Ok(ast::Stmt::If {
+          cond: cond,
+          true_stmt: Box::new(true_stmt),
+          false_stmt: false_stmt.map(|s| Box::new(s)),
+        })
+      }
+      _ => {
+        let stmt = ast::Stmt::Expr(self.parse_expr(1)?);
+        self.expect(TokenKind::Semicolon)?;
+        Ok(stmt)
+      }
+    }
+  }
+
+  fn parse_factor(&self) -> Result<ast::Expr, Error> {
     todo!()
   }
 }
